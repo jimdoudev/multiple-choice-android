16d9e78472d325f5917ab7feb91849d1636c4030
Dimitrios Doumpis <doumpis@hotmail.com>
2022-04-04 22:17:54 +0300

    Data Entry Activity: Layout and Go  Button

---
+++ app/src/main/java/com/example/multiplechoiceproject/MultipleChoice.java
@@ -0,0 +1,14 @@
+package com.example.multiplechoiceproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Questionnaire extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questionnaire);
+    }
+}
